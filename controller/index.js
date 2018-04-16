ad252fae00ae098a8fd84457618479fbeec27809
1tyler1 <mya12ga@yahoo.com>
2018-04-16 10:54:31 -0400

    updating projects page for full CRUD

--- controller/index.js
+++ controller/index.js
@@ -57,7 +57,7 @@ router.get('/:id/projects', function(req, res) {
 
 
 router.get('/:id/projects/edit', function(req, res) {
-    const id = req.params.projectsId;
+    const id = req.params.projects_id;
 
     projects.findByIdAndUpdate(id)
         .then(projects => {
